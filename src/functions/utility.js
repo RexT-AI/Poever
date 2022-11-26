45453ff69b53e29fae901176aef1b90727ba508d
kwakrhkr59 <89788679+kwakrhkr59@users.noreply.github.com>
2022-11-27 02:49:26 +0900

    각도 계산 함수[11270249]

--- src/functions/utility.js
+++ src/functions/utility.js
@@ -1,4 +1,4 @@
-const SCORE_THRESHOLD = 0.3; // 각도 계산 임계점
+const SCORE_THRESHOLD = 0.2; // 각도 계산 임계점
 const CMP_THRESHOLD = 0.15;  // 각도 비교 임계점
 /**
  * p1-p2-p3 사이각 반환 (양수: 예각, 음수: 둔각)
@@ -29,8 +29,8 @@ export function getSide(pred) {
     const left_ear = pred[0][17];
 
     console.log("hip");
-    if (left_hip != undefined && right_hip === undefined) return "right";
-    else if (left_hip === undefined && right_hip != undefined) return "left";
+    if (left_hip != undefined && right_hip === undefined) return "left";
+    else if (left_hip === undefined && right_hip != undefined) return "right";
     else if (left_hip != undefined && right_hip != undefined) {
         const tmp = right_hip.score - left_hip.score;
         if (Math.abs(tmp) > CMP_THRESHOLD) {
@@ -39,8 +39,8 @@ export function getSide(pred) {
     }
 
     console.log("shoulder");
-    if (left_shoulder != undefined && right_shoulder === undefined) return "right";
-    else if (left_shoulder === undefined && right_shoulder != undefined) return "left";
+    if (left_shoulder != undefined && right_shoulder === undefined) return "left";
+    else if (left_shoulder === undefined && right_shoulder != undefined) return "right";
     else if (left_shoulder != undefined && right_shoulder != undefined) {
         const tmp = right_shoulder.score - left_shoulder.score;
         if (Math.abs(tmp) > CMP_THRESHOLD) {
@@ -49,12 +49,12 @@ export function getSide(pred) {
     }
 
     console.log("ear");
-    if (left_ear === undefined && right_ear != undefined) return "right";
-    else if (left_ear != undefined && right_ear === undefined) return "left";
+    if (left_ear === undefined && right_ear != undefined) return "left";
+    else if (left_ear != undefined && right_ear === undefined) return "right";
     else if (left_ear != undefined && right_ear != undefined) {
         const tmp = right_ear.score- left_ear.score;
         if (Math.abs(tmp) > CMP_THRESHOLD) {
-            return (tmp > 0) ? "right" : "left";
+            return (tmp > 0) ? "left" : "right";
         }
     }
 
@@ -81,7 +81,7 @@ export function func1(pred, side) {
         if (right_hip < SCORE_THRESHOLD || nose < SCORE_THRESHOLD || right_ear < SCORE_THRESHOLD ) {
             return undefined;
         }
-        return getAngle(right_hip, nose, right_ear);
+        return getAngle(right_ear, nose, right_hip);
     }
     // 왼쪽: 17-0-11
     if (left_ear === undefined || nose === undefined || left_hip === undefined) {
@@ -90,7 +90,7 @@ export function func1(pred, side) {
     if (left_ear < SCORE_THRESHOLD || nose < SCORE_THRESHOLD || left_hip < SCORE_THRESHOLD ) {
         return undefined;
     }
-    return getAngle(left_ear, nose, left_hip);
+    return getAngle(left_hip, nose, left_ear);
 }
 /**
  * 허리 숙인 각 계산
@@ -112,7 +112,7 @@ export function func2(pred, side) {
         if (right_knee < SCORE_THRESHOLD || right_hip < SCORE_THRESHOLD || neck < SCORE_THRESHOLD ) {
             return undefined;
         }
-        return getAngle(right_knee, right_hip, neck);
+        return getAngle(neck, right_hip, right_knee);
     }
     // 왼쪽: 1-11-12
     if (neck === undefined || left_hip === undefined || left_knee === undefined) {
@@ -121,7 +121,7 @@ export function func2(pred, side) {
     if (neck < SCORE_THRESHOLD || left_hip < SCORE_THRESHOLD || left_knee < SCORE_THRESHOLD ) {
         return undefined;
     }
-    return getAngle(neck, left_hip, left_knee);
+    return getAngle(left_knee, left_hip, neck);
 }
 /**
  * 다리 꼬았는지 여부 반환
@@ -136,15 +136,16 @@ export function func3(pred, side) {
     const left_ankle = pred[0][13];
 
     if (side == "right") {
-        if (left_knee === undefined) return false;
+        if (left_knee === undefined) { console.log("31"); return false; }
         if (right_knee === undefined || left_knee.score > right_knee + CMP_THRESHOLD) {
             return true;
         }
-        if (left_ankle === undefined) return false;
+        if (left_ankle === undefined) { console.log("32"); return false; }
         if (right_ankle === undefined && left_ankle.score > SCORE_THRESHOLD) {
             return true;
         }
-        return false;
+        
+        return true;
     }
 
     if (right_knee === undefined) return false;
@@ -155,7 +156,7 @@ export function func3(pred, side) {
     if (left_ankle === undefined && right_ankle.score > SCORE_THRESHOLD) {
         return true;
     }
-    return false;
+    return true;
 }
 /**
  * 무릎 접은 각 계산
@@ -171,18 +172,21 @@ export function func4(pred, side) {
     const left_knee = pred[0][12];
     const left_ankle = pred[0][13];
     // 다리를 꼰 경우(func4!=undefined) 무릎 각도 계산 X
-    if (func2(pred, side) != undefined) return undefined;
+    if (func3(pred, side) != false) { console.log("41"); return undefined; }
     // 오른쪽: 8-9-10
     if (side == "right") {
         if (right_hip === undefined || right_knee === undefined || right_ankle === undefined) {
+            console.log("42"); 
             return undefined;
         }
         if (right_hip < SCORE_THRESHOLD || right_knee < SCORE_THRESHOLD || right_ankle < SCORE_THRESHOLD ) {
+            console.log("43"); 
             return undefined;
         }
-        return getAngle(right_hip, right_knee, right_ankle);
+        console.log("44"); 
+        return getAngle(right_ankle, right_knee, right_hip);
     }
-    // 오른쪽: 13-12-11
+    // 왼쪽: 13-12-11
     if (left_ankle === undefined || left_knee === undefined || left_hip === undefined) {
         return undefined;
     }
@@ -190,7 +194,7 @@ export function func4(pred, side) {
     if (left_ankle < SCORE_THRESHOLD || left_knee < SCORE_THRESHOLD || left_hip < SCORE_THRESHOLD ) {
         return undefined;
     }
-    return getAngle(left_ankle, left_knee, left_hip);
+    return getAngle(left_hip, left_knee, left_ankle);
 }
 export function getPose(pred) {
     const side = getSide(pred);
